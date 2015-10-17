28dd98171a50f68394cdf86c509f3326b3458c06
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-10-17 18:51:34 -0400

    Init Plugin File

---
+++ jordanpak-functionality.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: JordanPak Theme Functionality
+Plugin URI:  http://jordanpak.com/
+Description: Custom post types, shortcodes, and other functionality for the JordanPak Genesis child theme.
+Version:     1.0
+Author:      Jordan Pakrosnis
+Author URI:  http://JordanPak.com/
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
