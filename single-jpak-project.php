bbea8980f346c95cc76844155d98a6b9f8434fe7
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-10-18 10:47:03 -0400

    Init Project Single Template

---
+++ single-jpak-project.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * JordanPak Functionality - Project CPT Template - Single
+ *
+ * Single Template for Project CPT
+ *
+ * @package JordanPak-Functionality
+ * @since 1.0.0
+ */
+
+
+
+
+//-- LOAD FRAMEWORK --//
+genesis();
