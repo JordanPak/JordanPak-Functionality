cc4767aa7ecdf3f26a2cada40c381115cfce8871
Jordan Pakrosnis <Jpak@JpakMedia.com>
2015-10-20 15:57:19 -0400

    Replace Grid Loop with new "easier" one

--- archive-jpak-project.php
+++ archive-jpak-project.php
@@ -17,7 +17,10 @@
 
 //-- REMOVE DEFAULT ELEMENTS --//
 remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
-remove_action( 'genesis_loop', 'genesis_do_loop' );
+// remove_action( 'genesis_loop', 'genesis_do_loop' );
+remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
 
 
 //-- ADD BODY CLASSES --//
@@ -38,6 +41,21 @@ function jpak_project_archive_body_classes( $classes ) {
     return $classes;
 
 } // jpak_project_archive_body_classes
+
+
+
+function be_archive_post_class( $classes ) {
+	global $wp_query;
+	if( ! $wp_query->is_main_query() )
+		return $classes;
+
+	$classes[] = 'one-half';
+	if( 0 == $wp_query->current_post || 0 == $wp_query->current_post % 2 )
+		$classes[] = 'first';
+	return $classes;
+}
+add_filter( 'post_class', 'be_archive_post_class' );
+
 
 
 //-- LOAD FRAMEWORK --//
